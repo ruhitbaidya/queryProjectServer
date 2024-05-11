6cd26b2b75da30042c61f7f4e5c94904ba6391e6
ruhitbaidya <ruhitbaidya01@gmail.com>
2024-05-11 10:00:03 +0600

    product create router complate

--- index.js
+++ index.js
@@ -6,6 +6,58 @@ app.use(express.json());
 app.use(cors());
 
 
+// password
+// PVOatlalzfcaRiaF
+// userName
+// product_alternative
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://product_alternative:PVOatlalzfcaRiaF@datafind.xfgov3s.mongodb.net/?retryWrites=true&w=majority&appName=datafind";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+    const datacoll = client.db("alternativeProduct").collection("product");
+
+    app.post("/createProduct", async(req, res)=>{
+        const prods = req.body;
+        try{
+            const result = await datacoll.insertOne(prods)
+            res.send(result)
+        }
+        catch(err){
+            res.send(err.message)
+        }
+    })
+
+
+
+
+
+
+
+
+
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
