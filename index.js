3cc7a6a6fab669afb604470cb6f977a67919e0e1
ruhitbaidya <ruhitbaidya01@gmail.com>
2024-05-12 18:27:14 +0600

    finish jwt work and verification

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser")
 require("dotenv").config()
+const jwt = require("jsonwebtoken");
 const app = express();
 
 app.use(express.json());
 app.use(
-    cors({
-      origin: [
-        "http://localhost:5173",
-        "https://cardoctor-bd.web.app",
-        "https://cardoctor-bd.firebaseapp.com",
-      ],
-      credentials: true,
-    })
-  );
-
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://cardoctor-bd.web.app",
+      "https://cardoctor-bd.firebaseapp.com",
+    ],
+    credentials: true,
+  })
+);
+app.use(cookieParser())
 
 // password
 // PVOatlalzfcaRiaF
 // userName
 // product_alternative
-
+const cookieOptions = {
+  httpOnly: true,
+  secure: true,
+  sameSite: false,
+};
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@datafind.xfgov3s.mongodb.net/?retryWrites=true&w=majority&appName=datafind`;
+
+// if(req.decode === req.params.email){
+
+// }else{
+//   res.send({validation : "unauthorize user"})
+// }
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -36,145 +48,176 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    
+
     const datacoll = client.db("alternativeProduct").collection("product");
     const dataRecomendation = client.db("alternativeProduct").collection("recommendation");
 
-    app.post("/createProduct", async(req, res)=>{
-        const prods = req.body;
-        try{
-            const result = await datacoll.insertOne(prods)
-            res.send(result)
+    const verify = ((req, res, next) => {
+      const findtoken = req.cookies.token;
+      jwt.verify(findtoken, process.env.JWT_SECRATE, (err, decode) => {
+        if (err) {
+          res.send({ seccess: false, user: "UnAuthroize User" })
         }
-        catch(err){
-            res.send(err.message)
-        }
+        req.decode = decode
+        next()
+      })
     })
 
-    app.get('/findProduct', async(req, res)=>{
-        try{
-            const result = await datacoll.find().toArray();
-            res.send(result)
-        }
-        catch(err){
-            res.send(err.message)
-        }
+    app.get("/jwtTokenCreate/:email", async (req, res) => {
+      const emails = req.params.email;
+      const token = jwt.sign(emails, process.env.JWT_SECRATE);
+      res.cookie("token", token, cookieOptions).send({ success: true })
     })
 
-    app.get("/getProductByEmail", async(req, res)=>{
-        try{
-            const email = {"userinfotime.userEmail" : req.query.email}
-            const result = await datacoll.find(email).toArray();
-            console.log(result)
-            res.send(result)
-        }
-        catch(err){
-            res.send(err.message)
-        }
-    })
+    app.post("/createProduct", verify, async (req, res) => {
 
-
-    app.get("/findData/:id", async(req, res)=>{
-            try{
-                const ids = {_id : new ObjectId(req.params.id)}
-                const result = await datacoll.findOne(ids);
-                res.send(result)
-            }
-            catch(err){
-                res.send(err.message)
-            }
-    })
-
-    app.put("/updateProduct/:id", async(req, res)=>{
-        try{
-            const id = {_id : new ObjectId(req.params.id)}
-            const find = req.body;
-            const options = {
-              $set : find
-            }
-            console.log(options)
-            const result = await datacoll.updateOne(id, options);
-            res.send(result)
-
-        }
-        catch(err){
-            res.send(err.message)
-        }
-    })
-
-    app.delete("/delete/:id", async(req, res)=>{
-        try{
-          const ids = {_id : new ObjectId(req.params.id)};
-          const result = await datacoll.deleteOne(ids)
+      const prods = req.body;
+      try {
+        if (req.decode === prods.userinfotime.userEmail) {
+          const result = await datacoll.insertOne(prods)
           res.send(result)
-        } 
-        catch(err){
-          res.send(err.message)
+        } else {
+          res.send({ validation: "unauthorize user", message: "user Not Fount : Unauthorize User" })
         }
-    })
-
-    app.post("/comment", async(req, res)=>{
-        console.log(req.body)
-        const idscount = {_id : new ObjectId(req.body.queryId)};
-        const increment = {$inc : {"userinfotime.recommendationCount" : 1}}
-        await datacoll.updateOne(idscount, increment);
-        const datas = req.body;
-        const result = await dataRecomendation.insertOne(datas);
-        res.send(result);
-    })
-
-    app.get("/allcomment/:id", async(req, res)=>{
-        try{
-          const idsfi = {queryId : req.params.id}
-          const result = await dataRecomendation.find(idsfi).toArray();
-          res.send(result)
-        }
-        catch(err){
-          res.send(err.message)
-        }
-    })
-
-    app.get("/mycomment/:id", async(req, res)=>{
-      
-      try{  
-        const email = {reEmail : req.params.id}
-        const result = await dataRecomendation.find(email).toArray();
-        res.send(result)
       }
-      catch(err){
+      catch (err) {
         res.send(err.message)
       }
     })
 
-    app.post("/deleteProduct/:id", async(req, res)=>{
-      try{
-        const ids = {_id : new ObjectId(req.params.id)}
-        const idscount = {_id : new ObjectId(req.body.id)};
-        const increment = {$inc : {"userinfotime.recommendationCount" : -1}}
+    app.get('/findProduct', async (req, res) => {
+      try {
+        const result = await datacoll.find().toArray();
+        res.send(result)
+      }
+      catch (err) {
+        res.send(err.message)
+      }
+    })
+
+    app.get("/getProductByEmail", async (req, res) => {
+      try {
+        const email = { "userinfotime.userEmail": req.query.email }
+        const result = await datacoll.find(email).toArray();
+        console.log(result)
+        res.send(result)
+      }
+      catch (err) {
+        res.send(err.message)
+      }
+    })
+
+
+    app.get("/findData/:id", async (req, res) => {
+      try {
+        const ids = { _id: new ObjectId(req.params.id) }
+        const result = await datacoll.findOne(ids);
+        res.send(result)
+      }
+      catch (err) {
+        res.send(err.message)
+      }
+    })
+
+    app.put("/updateProduct/:id", async (req, res) => {
+      try {
+        const id = { _id: new ObjectId(req.params.id) }
+        const find = req.body;
+        const options = {
+          $set: find
+        }
+        console.log(options)
+        const result = await datacoll.updateOne(id, options);
+        res.send(result)
+
+      }
+      catch (err) {
+        res.send(err.message)
+      }
+    })
+
+    app.delete("/delete/:id", async (req, res) => {
+      try {
+        const ids = { _id: new ObjectId(req.params.id) };
+        const result = await datacoll.deleteOne(ids)
+        res.send(result)
+      }
+      catch (err) {
+        res.send(err.message)
+      }
+    })
+
+    app.post("/comment", async (req, res) => {
+      console.log(req.body)
+      const idscount = { _id: new ObjectId(req.body.queryId) };
+      const increment = { $inc: { "userinfotime.recommendationCount": 1 } }
+      await datacoll.updateOne(idscount, increment);
+      const datas = req.body;
+      const result = await dataRecomendation.insertOne(datas);
+      res.send(result);
+    })
+
+    app.get("/allcomment/:id", async (req, res) => {
+      try {
+        const idsfi = { queryId: req.params.id }
+        const result = await dataRecomendation.find(idsfi).toArray();
+        res.send(result)
+      }
+      catch (err) {
+        res.send(err.message)
+      }
+    })
+
+    app.get("/mycomment/:id", async (req, res) => {
+
+      try {
+        const email = { reEmail: req.params.id }
+        const result = await dataRecomendation.find(email).toArray();
+        res.send(result)
+      }
+      catch (err) {
+        res.send(err.message)
+      }
+    })
+
+    app.post("/deleteProduct/:id", async (req, res) => {
+      try {
+        const ids = { _id: new ObjectId(req.params.id) }
+        const idscount = { _id: new ObjectId(req.body.id) };
+        const increment = { $inc: { "userinfotime.recommendationCount": -1 } }
         await datacoll.updateOne(idscount, increment);
         const result = await dataRecomendation.deleteOne(ids);
         res.send(result)
       }
-      catch(err){
+      catch (err) {
         res.send(err.message)
       }
     })
-    app.get("/getOtherComment/:email", async(req, res)=>{
-        try{
-          const email = req.params.email;
-          const result = await dataRecomendation.find({reEmail : {$ne : email}}).toArray()
-          res.send(result);
-        }
-        catch(err){
-          res.send(err.message)
-        }
+    app.post("/logutUser", async (req, res) => {
+        res.clearCookie("token", { cookieOptions, maxAge: 0 })
+        .send({ success: true });
     })
-  
-    app.get("/searchproduct/:text", async(req, res)=>{
-        const text = req.params.text;
-        const filter = { productName: { $regex: text, $options: 'i' } };
-        const result = await datacoll.find(filter).toArray();
-        res.send(result)
+    app.get("/getOtherComment/:email", verify, async (req, res) => {
+      try {
+        if (req.decode === req.params.email) {
+          const email = req.params.email;
+          const result = await dataRecomendation.find({ reEmail: { $ne: email } }).toArray()
+          res.send(result);
+        } else {
+          res.send({ validation: "unauthorize user" })
+        }
+
+      }
+      catch (err) {
+        res.send(err.message)
+      }
+    })
+
+    app.get("/searchproduct/:text", async (req, res) => {
+      const text = req.params.text;
+      const filter = { productName: { $regex: text, $options: 'i' } };
+      const result = await datacoll.find(filter).toArray();
+      res.send(result)
     })
     await client.connect();
     // Send a ping to confirm a successful connection
@@ -189,6 +232,6 @@ run().catch(console.dir);
 
 
 
-app.listen(5000, ()=>{
-    console.log("this server is running")
+app.listen(5000, () => {
+  console.log("this server is running")
 })
