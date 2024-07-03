e62e17c86dff45a64acb005622fea728a0c78f95
ruhitbaidya <ruhitbaidya01@gmail.com>
2024-07-03 19:02:28 +0600

    this all code change

--- index.js
+++ index.js
@@ -3,44 +3,30 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
 const app = express();
 app.use(express.json());
-<<<<<<< HEAD
-=======
+app.use(cookieParser());
 
-app.use(cors({
+// CORS configuration
+const corsOptions = {
   origin: [
     "http://localhost:5173",
     "https://alternative-product-bb915.web.app"
   ],
-  credentials: true,
-}));
-
-
->>>>>>> 76575efa71c508250feaf6ef2a70e7d00db10520
-app.use(cookieParser());
-const corsOptions = {
-  origin: 'http://localhost:5173',
-  credentials: true // This allows cookies and other credentials to be sent
+  credentials: true
 };
-
-<<<<<<< HEAD
 app.use(cors(corsOptions));
-
-// Handle preflight requests
 app.options('*', cors(corsOptions));
-=======
-const stripe = require("stripe")(process.env.STRIPE_KEY);
 
->>>>>>> 76575efa71c508250feaf6ef2a70e7d00db10520
 const cookieOptions = {
   httpOnly: true,
   secure: true,
   sameSite: true,
 };
 
-
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@datafind.xfgov3s.mongodb.net/?retryWrites=true&w=majority&appName=datafind`;
 
 const client = new MongoClient(uri, {
@@ -62,16 +48,7 @@ async function run() {
     const verify = (req, res, next) => {
       try {
         const findtoken = req.cookies.token;
-
-<<<<<<< HEAD
-        if (findtoken === null) {
-          res.send({ status: "findUnAuthorize User" });
-        }
-        jwt.verify(findtoken, process.env.JWT_SECRATE, (err, decode) => {
-          if (err) {
-            res.send({ success: false, user: "errUnAuthroize User" });
-=======
-        if (findtoken === undefined) {
+        if (!findtoken) {
           return res.send({ status: "findUnAuthorize User" });
         }
         jwt.verify(findtoken, process.env.JWT_SECRATE, (err, decode) => {
@@ -80,37 +57,33 @@ async function run() {
           } else {
             req.decode = decode;
             next();
->>>>>>> 76575efa71c508250feaf6ef2a70e7d00db10520
           }
         });
       } catch (err) {
         res.send({ success: false, message: err.message });
       }
-    };
+    }
 
-    // payment integreat
+    // Payment integration
+    app.post("/payment-create", async (req, res) => {
+      const price = req.body;
+      const num = parseInt(price.money * 100);
 
-    app.post("/payment-create", async(req, res)=>{
-        const price = req.body;
+      const paymentIngreat = await stripe.paymentIntents.create({
+        amount: num,
+        currency: "usd",
+        automatic_payment_methods: {
+          enabled: true,
+        },
+      });
 
-        const num = parseInt(price.money * 100);
-        
-        const paymentIngreat = await stripe.paymentIntents.create({
-            amount : 200,
-            currency : "usd",
-            automatic_payment_methods: {
-              enabled: true,
-            },
-        })
-
-        res.send({clientSecrate : paymentIngreat.client_secret});
-    })
- 
+      res.send({ clientSecrate: paymentIngreat.client_secret });
+    });
 
     app.get("/jwtTokenCreate/:email", async (req, res) => {
       try {
-        const emails = req.params.email;
-        const token = jwt.sign(emails, process.env.JWT_SECRATE);
+        const email = req.params.email;
+        const token = jwt.sign(email, process.env.JWT_SECRATE);
         res.cookie("token", token, cookieOptions).send({ success: true });
       } catch (err) {
         res.send({ success: false, message: err.message });
@@ -119,29 +92,20 @@ async function run() {
 
     app.get("/productGetCard", verify, async (req, res) => {
       const result = await addcard.find().toArray();
-      res.send(result);
+      return res.send(result);
     });
+
     app.delete("/deleteCardProduct/:id", async (req, res) => {
-<<<<<<< HEAD
-=======
-      console.log(req.params.id);
->>>>>>> 76575efa71c508250feaf6ef2a70e7d00db10520
       const ids = { _id: new ObjectId(req.params.id) };
       const result = await addcard.deleteOne(ids);
       res.send(result);
     });
-<<<<<<< HEAD
-    app.post("/createProductAlternative", async (req, res) => {
-=======
-    app.post("/createProduct", verify, async (req, res) => {
->>>>>>> 76575efa71c508250feaf6ef2a70e7d00db10520
+
+    app.post("/createProductAlternative", verify, async (req, res) => {
       try {
         const prods = req.body;
-
-          const result = await datacoll.insertOne(prods);
-          return res.send(result);
-       
-      
+        const result = await datacoll.insertOne(prods);
+        res.send(result);
       } catch {
         res.send({ message: "server error" });
       }
@@ -152,6 +116,7 @@ async function run() {
       const result = await addcard.insertOne(product);
       res.send(result);
     });
+
     app.get("/findProduct", async (req, res) => {
       try {
         const result = await datacoll.find().toArray();
@@ -166,9 +131,9 @@ async function run() {
         if (req.decode === req.query.email) {
           const email = { "userinfotime.userEmail": req.query.email };
           const result = await datacoll.find(email).toArray();
-          return res.send(result);
+          res.send(result);
         } else {
-          return res.send({ error: "router : Invalid User" });
+          res.send({ error: "router: Invalid User" });
         }
       } catch (err) {
         res.send(err.message);
@@ -255,17 +220,15 @@ async function run() {
         res.send(err.message);
       }
     });
+
     app.get("/logutUser", async (req, res) => {
-      res
-        .clearCookie("token", { cookieOptions, maxAge: 0 })
-        .send({ success: true });
+      res.clearCookie("token", { cookieOptions, maxAge: 0 }).send({ success: true });
     });
+
     app.get("/getOtherComment/:email", async (req, res) => {
       try {
         const email = req.params.email;
-        const result = await dataRecomendation
-          .find({ reEmail: { $ne: email } })
-          .toArray();
+        const result = await dataRecomendation.find({ reEmail: { $ne: email } }).toArray();
         res.send(result);
       } catch (err) {
         res.send(err.message);
@@ -279,9 +242,7 @@ async function run() {
       res.send(result);
     });
 
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -290,5 +251,5 @@ async function run() {
 run().catch(console.dir);
 
 app.listen(5000, () => {
-  console.log("this server is running");
+  console.log("This server is running");
 });
